2f1e88ebad6d65b83c7f460d1796ec7fcedd86a3
Ryan Ghaida <ryaboug@gmail.com>
2019-10-15 13:45:29 -0400

    revised solution

--- addTwoLinkedListNums.js
+++ addTwoLinkedListNums.js
@@ -1,21 +1,24 @@
 var addTwoNumbers = function(l1, l2) {
-  //reverse arrays, l1 -> [2,4,3] -> 342
-  //use length attr of array to explicitly pull numbers 
-  const arr1 = [];
-  const arr2 = [];
-
-    for(let i = 0; i < l1.length; i++){             arr1.unshift(l1[i])
-    };
-
-    for(let i = 0; i < l2.length; i++){
-      arr2.unshift(l2[i])
-    }
-
-  let sum = parseInt(arr1.join("")) + parseInt(arr2.join(""))
-  sum = sum.toString().split("");
-  const result = []; 
-  for(let i = 0; i < sum.length; i++){             result.unshift(sum[i])
-    };
-  return result
+	let dummy = new ListNode(0);
+	let cur = dummy;
+	let sum = 0;
+	let p1 = l1;
+	let p2 = l2;
+	while (p1 != null || p2 != null) {
+		if (p1 != null) {
+			sum += p1.val;
+			p1 = p1.next;
+		}
+		if (p2 != null) {
+			sum += p2.val;
+			p2 = p2.next;
+		}
+		cur.next = new ListNode(sum % 10);
+		sum = parseInt(sum / 10);
+		cur = cur.next;
+	}
+	if (sum === 1) {
+		cur.next = new ListNode(1);
+	}
+	return dummy.next;
 };
-addTwoNumbers([2,4,3], [5,6,4])
