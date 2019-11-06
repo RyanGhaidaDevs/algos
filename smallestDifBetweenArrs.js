226c5d731be7ce664d3a3bea5a71ec0d3bce22a6
Ryan Ghaida <ryaboug@gmail.com>
2019-11-06 15:29:34 -0500

    better solution using abs values

--- smallestDifBetweenArrs.js
+++ smallestDifBetweenArrs.js
@@ -1,26 +1,26 @@
 //find the smallest difference between two arrays of integers. 
-
 function smallestDif(arr1, arr2){
-const solution = [];
-const hash = {}
-const min = arr1[0];
-
-
-arr1.sort((a,b)=> a-b);
-arr2.sort((a,b)=> a-b);
-
-for(let i = 0; i<arr1.length;i++ ){
-  for(let j =0; j < arr2.length; j++){
-    let currentCombo = [arr1[i], arr2[j]];
-    let currentDif = arr1[i]-arr2[j]; 
-    if(!(currentDif in hash)){
-      hash[Math.abs(currentDif)] = [[arr1[i], arr2[j]]]
-    } else {
-      hash[currentDif].push([arr1[i], arr2[j]])
+  const solution = [];
+  const hash = {}
+  const min = arr1[0];
+  
+  
+  arr1.sort((a,b)=> a-b);
+  arr2.sort((a,b)=> a-b);
+  
+  for(let i = 0; i<arr1.length;i++ ){
+    for(let j =0; j < arr2.length; j++){
+      let currentCombo = [arr1[i], arr2[j]];
+      let currentDif = Math.abs(arr1[i]) - Math.abs(arr2[j]); 
+      console.log(Math.abs(arr1[i]), Math.abs(arr2[j]), Math.abs(currentDif))
+      if(!(currentDif in hash) && i === 0&& j === 0 ){
+        hash[Math.abs(currentDif)] = [[arr1[i], arr2[j]]]
+      } else if (Math.abs(currentDif) < Object.keys(hash)[0]) {
+        hash[Math.abs(currentDif)] = [[arr1[i], arr2[j]]]
+      }
     }
   }
-}
-return hash[Object.keys(hash)[0]][0]
-}
-smallestDif([-1, 5, 10, 20, 28, 3], [26, 134, 135, 15, 17])
-//[28,26]
+  return hash[Object.keys(hash)[0]][0]
+  }
+  
+  smallestDif([-1, 5, 10, 20, 28, 3], [26, 134, 135, 15, 17])
