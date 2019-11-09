4dab6767b10c84c82d7a14892c621b8f96d59c63
Ryan Ghaida <ryaboug@gmail.com>
2019-11-09 13:17:06 -0500

    find three alrgest numbers in unsorted array

--- smallestDifBetweenArrs.js
+++ smallestDifBetweenArrs.js
@@ -11,12 +11,11 @@ function smallestDif(arr1, arr2){
   for(let i = 0; i<arr1.length;i++ ){
     for(let j =0; j < arr2.length; j++){
       let currentCombo = [arr1[i], arr2[j]];
-      let currentDif = Math.abs(arr1[i]) - Math.abs(arr2[j]); 
-      console.log(Math.abs(arr1[i]), Math.abs(arr2[j]), Math.abs(currentDif))
+      let currentDif = Math.abs(arr1[i] - arr2[j]); 
       if(!(currentDif in hash) && i === 0&& j === 0 ){
-        hash[Math.abs(currentDif)] = [[arr1[i], arr2[j]]]
-      } else if (Math.abs(currentDif) < Object.keys(hash)[0]) {
-        hash[Math.abs(currentDif)] = [[arr1[i], arr2[j]]]
+        hash[currentDif] = [[arr1[i], arr2[j]]]
+      } else if (currentDif < Object.keys(hash)[0]) {
+        hash[currentDif] = [[arr1[i], arr2[j]]]
       }
     }
   }
