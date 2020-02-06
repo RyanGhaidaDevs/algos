c3a6648c6dc453e65740569971c41bd9fbad5ae3
Ryan Ghaida <ryaboug@gmail.com>
2020-02-06 17:31:30 -0500

    added comments

--- Linked Lists/reverseLinkedList.js
+++ Linked Lists/reverseLinkedList.js
@@ -6,11 +6,21 @@ function reverseLinkedList(head){
 
   while(p2 !== null){
     const p3 = p2.next; 
+    p2.next = p1; 
     p1 = p2; 
     p2 = p3;
   }
   return p1 
 }
+//[1]->[2]->[3]->[4]
+//we dont want to lose access to p2 so we first set p3 = p2.next.
+//reverse pointer of p2 <-- 
+//move p1 to the right to be p2
+//move p2 to the right to be p3. 
+//continue untill end. 
+
+
+
 
 
   
