5abcd87a74fb71c9454882953219623c643f95b8
Ryan Ghaida <ryaboug@gmail.com>
2020-02-16 12:44:25 -0500

    added explanation

--- Linked Lists/findLoopinLL.js
+++ Linked Lists/findLoopinLL.js
@@ -25,3 +25,20 @@ function findLoop(head){
   }
   return first; 
 }
+
+//assume distance to loop start is D 
+//distance first pointer travels after D and when it hits second pointer is P
+//distance from P to the beggining of the loop is R. 
+
+//first pointer traveled a total of D + P 
+//second pointer went twice as much so 2D + 2P 
+
+//total distance of LL is D + P + R 
+// T = D + P + R 
+//also for second pointer, T = 2D + 2P - P so, T = 2D + P 
+//thus, T = T and 2D + P = D + P + R 
+
+// R = D 
+
+//reset first pointer to head and move each pointer by one untill they intersect 
+//where they intersect is the beggining of the loop. 
