2e7eed2af46b2a270a0084f61f9fb1212cddc987
Ryan Ghaida <ryaboug@gmail.com>
2020-02-19 18:07:00 -0500

    BST iterative solution

--- BST/BSTConstruction.js
+++ BST/BSTConstruction.js
@@ -85,3 +85,37 @@ class BST{
   }
 }
 
+
+//Iterative Solution: 
+
+class BST{
+  constructor(value){
+    this.value = value; 
+    this.right = null; 
+    this.left = null;
+  }
+
+  insert(value){
+    let currentNode = this; 
+    while(true){
+      if(value < currentNode.value){
+        if(currentNode.left === null){
+          currentNode.left = new BST(value);
+          break;
+        } else {
+          currentNode = currentNode.left; 
+        }
+      } else {
+        if(currentNode.right === null){
+          currentNode.right = new BST(value);
+          break;
+        } else{
+          currentNode = currentNode.right;
+        }
+      }
+    }
+    return this;
+  }
+
+
+}
