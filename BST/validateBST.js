7f4989367cea78521e309d211adbc5ca7dbfce14
Ryan Ghaida <ryaboug@gmail.com>
2020-02-25 16:11:08 -0500

    potential solution

--- BST/validateBST.js
+++ BST/validateBST.js
@@ -8,6 +8,15 @@ class BST{
   }
 }
 
+
+//O(N) time and O(d) space where d is the depth of the BST
 function validateBST(tree){
-  
+  return validatesBSTHelper(tree, -Infinity, Infintiy)
+}
+
+function validateBSTHelper(tree, minVal, maxVal){
+  if(tree === null) return true;
+  if(tree.value < minVal || tree.value >= maxVal) return false;
+  const leftIsValid = validateBSTHelper(tree.left, minVal, tree.value);
+  return leftIsValid && validatesBSTHelper(tree.right, tree.value, maxVal);
 }
