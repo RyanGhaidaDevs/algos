007b73e3366d178617cc3fb68b7a3f35c4b403ca
Ryan Ghaida <ryaboug@gmail.com>
2020-03-03 13:16:37 -0500

    letter traversal algo

---
+++ Strings/letterTraverse.js
@@ -0,0 +1,8 @@
+//write a function that accepts two parameters; one being a non-empty string of letters and the second being a key of an integer value.
+//the key value should represent how many values each letter should shift by.
+//for example 'abc', 2 => 'cde'
+
+
+//initially I was thinking to make a LL of all the alphabet letters
+// Having the LL be one giant loop in which z -> a
+//creating a traverse method for the LL and just having that initiate a loop that will iterate the pointers for each letter by the set amount of the key. 
